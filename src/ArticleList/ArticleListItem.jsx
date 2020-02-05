83f4be342429872873d72da3c13488c32ae17679
Ramya Hebbar <hebbar.r@husky.neu.edu>
2020-02-05 13:48:25 -0800

    Edits after discussion with TA

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -5,16 +5,7 @@ import PropTypes from "prop-types";
 const ArticleListItem = props => {
 
 	return (
-		<html lang="en">
-
-		<head>
-			<meta charset="UTF-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-            <title>Week-4</title>
-		</head>
-
-		<body>
+		<div>
 			
 			<h4>{props.value.title}</h4>
 
@@ -25,10 +16,8 @@ const ArticleListItem = props => {
 	    			show article slug
 	  			</button>
 	  		</p>
-			
-		</body>
-
-		</html>
+	  		
+		</div>
 	);
 }
 
